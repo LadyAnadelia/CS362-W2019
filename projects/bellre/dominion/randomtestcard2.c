5caefd67c42ad1fc208da01ff379af7d237c4d2d
LadyAnadelia <bellre@oregonstate.edu>
2019-07-29 16:04:01 -0700

    Finished minion card for assignment 4

--- projects/bellre/dominion/randomtestcard2.c
+++ projects/bellre/dominion/randomtestcard2.c
@@ -1,4 +1,4 @@
-/*
+/*	MINION CARD
  * Name: Rebecca Bell
  * Description: random testing values for the dominion.c baron card
  */
@@ -12,7 +12,7 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define MAX_RUNS 500000
+#define MAX_RUNS 100000
 
 int main()
 {
@@ -20,13 +20,23 @@ int main()
 	int seed = 2000;
 	int numPlayer = 2;
 	int k[10] = {adventurer, tribute, estate, gardens, village, embargo, minion, mine, baron, smithy};
-	struct gameState G;
+	struct gameState G, testG;
 	
 
 	//values to be randomized
 	int choice1, choice2, choice;
-	int currentPlayer = 1;
+	int currentPlayer;
+	int nextPlayer = 1;
 	int handPos = 1;
+
+
+	//to test and see what values proved true
+	int passed;
+	int passedCount = 0;
+	int failureCoin = 0;
+	int failureDiscard = 0;
+	int failureNext = 0; // to check aginst nxtplayer handCount
+	int failureCurrent = 0; //to check against currentP handCount
 
 	//for the random values
 	srand(time(NULL));
@@ -44,9 +54,16 @@ int main()
 		//randomly select player
 		currentPlayer = rand() % 2;
 
+		//Random selection of next player
+		if(currentPlayer == 1)
+			nextPlayer = 0;
+
 		//change the spot in the cards where the one selected will be
 		handPos = rand() % 5;
 
+		passed = 1;
+		// didnot checke both values being on or off. 
+		// though it should be in code
 		//if statements switch between the two different options
 		if(choice == 0)
 		{
@@ -58,10 +75,67 @@ int main()
 			choice1 = 1;
 			choice2 = 0;
 		}
-	
+		
+		memcpy(&testG, &G, sizeof(struct gameState));
 		caseMinion(&G, choice1, choice2, handPos, currentPlayer);
+		
+		//set value to test against
+		testG.handCount[currentPlayer] = 4;
+		testG.handCount[nextPlayer] = 4;
 
+		//check number of coins increase by 2
+		if(choice1)
+		{
+			if((testG.coins + 2) != G.coins)
+			{
+				printf("Incorrect number of coins added\n");
+				failureCoin++;
+				passed = 0;
+			}
+		}
+
+		//changed values to occur becasue
+		else if(choice2)
+		{
+			if(numHandCards(&G) != 4) 
+			{
+				printf("Incorrect number discarded cards\n");
+				failureDiscard++;
+				passed = 0;
+			}
+			if(testG.handCount[currentPlayer] != G.handCount[currentPlayer])
+			{	
+				printf("Incorrect hand count for current player\n");
+				failureCurrent++;
+				passed = 0;
+			}
+
+			if(testG.handCount[nextPlayer] != G.handCount[nextPlayer])
+			{	
+				printf("Incorrect hand count for current player\n");
+				failureNext++;
+				passed = 0;
+			}
+
+		}
+		if(passed == 1)
+		{
+			passedCount++;
+		}
+		
 	}
+
+	printf("\n%d Tests Passed!\t", passedCount);
+	printf("%d coins count failed, ", failureCoin);
+	printf("%d discard count failed, ", failureDiscard);
+	printf("%d next player's hand count failed, ", failureNext);
+	printf("%d current player's hand count failed!\n", failureCurrent);
+
+	if(passedCount == MAX_RUNS)
+		printf("All Tests Pass for Minion Card!!!\n\n");
+	else
+		printf("There are errors in the code!!!\n\n");
+
 
 	return 0;
 }
